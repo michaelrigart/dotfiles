45f708cda9e6c7324414a773cf1aabd4a4c3b653
Michaël Rigart <michael@netronix.be>
2025-04-03 10:24:12 +0200

    create zsh cache dir
    Signed-off-by: Michaël Rigart <michael@netronix.be>

--- private_dot_config/private_macos/private_executable_provision.sh
+++ private_dot_config/private_macos/private_executable_provision.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 # Install by running /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/michaelrigart/dotfiles/refs/heads/main/private_dot_config/private_macos/private_executable_provision.sh)"
 
-RUBY_VERSION=3.3.5
+RUBY_VERSION=3.4.2
 PYTHON_VERSION=3.12.6
 
 while read "REPLY?Ready preparing for dotfile installation? .ssh keys must be present! [y]es|[n]o: " && [[ $REPLY != 'y' ]]; do
@@ -83,6 +83,7 @@ mise use -g python@$PYTHON_VERSION
 
 echo '========== Create cache directories =========='
 mkdir -p "${XDG_CACHE_HOME}/irb"
+mkdir -p "${XDG_CACHE_HOME}/zsh"
 
 echo '========== Cleanup =========='
 if [ -f "${HOME}/.zprofile" ]; then
@@ -101,5 +102,4 @@ fi
 
 
 
-rm .zshrc.pre-oh-my-zsh
 
